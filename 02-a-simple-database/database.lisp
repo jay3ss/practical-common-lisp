3f3f7dbb361e659569fd4ab02d3c3e8af32896d4
jay ess <mentoce@gmail.com>
2021-04-21 23:44:30 -0700

    Add function to prompt user to enter a CD

--- 02-a-simple-database/database.lisp
+++ 02-a-simple-database/database.lisp
@@ -21,3 +21,12 @@
     (format *query-io* "~a: " prompt)
     (force-output *query-io*)
     (read-line *query-io*))
+
+
+;;; Prompts the user to enter a CD
+(defun prompt-for-cd ()
+    (make-cd
+        (prompt-read "Title")
+        (prompt-read "Artist")
+        (or (parse-integer (prompt-read "Rating") :junk-allowed t) 0)
+        (y-or-n-p "Ripped [y/n]: ")))
